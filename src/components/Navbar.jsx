73aaa9115cf2bd972cd87a0d74eb1688b476bdf1
yusufkkaann <yusufkaanyucebilgic@gmail.com>
2024-02-16 20:44:36 +0300

    navmenu fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-function BasicExample() {
+function NavMenu() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -34,4 +34,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+export default NavMenu;
